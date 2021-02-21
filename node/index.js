6dee9a66b48fdc99f44418e948d2095f1a5103ed
Gabriel <gabriel.stamato@certsys.com.br>
2021-02-21 15:50:52 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database:  'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const createTable = `create table if not exists people(id int not null auto_increment, name varchar(255), primary key(id))`
+const sql = `INSERT INTO people(name) values('Gabriel Stamato')`
+const queryName = `select * from people`
+
+connection.query(createTable)
+connection.query(sql)
+
+let name = ''
+
+connection.query(queryName, function (error, results, fileds) {
+  if (error) throw error
+
+  name = results[0]
+})
+
+connection.end()
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Full Cycle </h1><br /> <h2>You're rock, ${name.name}`)
+})
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`)
+})
